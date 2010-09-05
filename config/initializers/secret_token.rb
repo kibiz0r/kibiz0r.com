2b7ca299cd6c160f1e70f10f880b8685787065d9
Michael Harrington <kibiz0r@gmail.com>
2010-09-05 12:15:17 -0400

    Downgraded to Rails 2.3.5.

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Kibiz0rCom::Application.config.secret_token = '646fd08bcd41e53459439ce3dcceb01251afe2e44fd33e38075cc9a580ac6b83fcfe849a5ecfe8c08f9c0f087c3eb4b14b78e5ce7e3dfa967387d20535ad5bc9'
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,8 +1,15 @@
 # Be sure to restart your server when you modify this file.
 
-Kibiz0rCom::Application.config.session_store :cookie_store, :key => '_kibiz0r.com_session'
+# Your secret key for verifying cookie session data integrity.
+# If you change this key, all old sessions will become invalid!
+# Make sure the secret is at least 30 characters and all random, 
+# no regular words or you'll be exposed to dictionary attacks.
+ActionController::Base.session = {
+  :key         => '_kibiz0r.com-downgrade_session',
+  :secret      => '6a72965a3890fa0d623137f84f829ec5119db41d002f707e6de833c5657df8255558cb450d101d415bc3c69d8a69eab89a425bdeaf4f0bf66f3aa7989b604b4d'
+}
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
 # (create the session table with "rake db:sessions:create")
-# Kibiz0rCom::Application.config.session_store :active_record_store
+# ActionController::Base.session_store = :active_record_store
