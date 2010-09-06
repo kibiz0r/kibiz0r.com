88fade5e909d3785f74ffc7933e05b4b1bc17731
Michael Harrington <kibiz0r@gmail.com>
2010-09-06 14:27:24 -0400

    More skills, refactored view.

--- app/controllers/skills_controller.rb
+++ app/controllers/skills_controller.rb
@@ -4,5 +4,40 @@ class SkillsController < ApplicationController
   end
 
   def languages
+    @items = [['C', 3],
+      ['C++', 3],
+      ['C#', 3],
+      ['Java', 2],
+      ['Visual Basic', 2],
+      ['F#', 1],
+      ['Ruby', 3],
+      ['Python', 2],
+      ['Boo', 2],
+      ['COBOL', 1],
+      ['JavaScript', 1],
+      ['ActionScript 3', 3],
+      ['TorqueScript', 2],
+      ['Axum', 2],
+      ['Objective-C', 2]]
+    render 'items'
+  end
+
+  def technologies
+    @items = [['Rails', 2],
+      ['iOS', 2],
+      ['.NET', 3],
+      ['WPF', 1],
+      ['Unity', 2],
+      ['Flash', 3],
+      ['Photoshop', 2],
+      ['Fireworks', 2]]
+    render 'items'
+  end
+
+  def misc
+    @items = [['Sailing', 3],
+      ['Guitar', 1],
+      ['Origami', 2]]
+    render 'items'
   end
 end
