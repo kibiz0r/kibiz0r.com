7739a0574634b707a2b46f2fc061aeed2e171964
Michael Harrington <kibiz0r@gmail.com>
2010-09-12 16:49:56 -0400

    Added Samurai Ninja Rabbit.

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -6,7 +6,8 @@ class ItemsController < ApplicationController
   def general
     @items = [
       ['Omega-VI', '/omega-vi.swf', 'Flash experiment with rock-paper-scissors dynamics, enemy patterns and communicating gameplay information without a UI.'],
-      ['Pisces Dentata', 'http://www.ludumdare.com/compo/ludum-dare-18/?action=preview&uid=2390', 'Ludum Dare 18 Jam entry, made in a weekend with some friends, using Flashpunk.']
+      ['Pisces Dentata', 'http://www.ludumdare.com/compo/ludum-dare-18/?action=preview&uid=2390', 'Ludum Dare 18 Jam entry, made in a weekend with some friends, using Flashpunk.'],
+      ['Samurai Ninja Rabbit', '/samurai-ninja-rabbit.html', 'A Flash game made in 24 sleep-deprived hours for FragFest at Ferris Grand Rapids. The movement system and randomly-generated infinite level turned out really well, but there\'s nothing to kill (yet).']
     ]
     render 'items'
   end
